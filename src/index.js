c50e583583f314510f76926871b8890e518526c2
Jurimeu <139811045+Jurimeu@users.noreply.github.com>
2024-10-05 22:21:45 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const sqlite3 = require('sqlite3').verbose();
+const express = require('express'); // importa o express
+const sqlite3 = require('sqlite3').verbose(); // importa os comandos pra usar o SQLite
 const cors = require('cors'); // Importar o pacote cors
 
 const app = express();
@@ -7,8 +7,8 @@ app.use(cors()); // Adicionar o middleware cors
 app.use(express.json());
 const port = 3000;
 
-// Criar ou abrir o banco de dados SQLite
-const db = new sqlite3.Database('./comidas.db', (err) => {
+// Criar ou abre o banco de dados SQLite (neste tete é usuarios.db)
+const db = new sqlite3.Database('./usuarios.db', (err) => {
     if (err) {
         console.error('Erro ao conectar ao banco de dados:', err.message);
     } else {
@@ -17,16 +17,16 @@ const db = new sqlite3.Database('./comidas.db', (err) => {
 });
 
 // Criar a tabela se não existir
-db.run(`CREATE TABLE IF NOT EXISTS comidas (
+db.run(`CREATE TABLE IF NOT EXISTS usuarios (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    gostosas TEXT,
-    suculentas TEXT,
-    image_url TEXT
+    nome TEXT,
+    email TEXT,
+    senha TEXT
 );`);
 
-// Rota GET para buscar todas as comidas
+// Rota GET para buscar todas usuarios
 app.get('/', (req, res) => {
-    db.all(`SELECT * FROM comidas`, [], (err, rows) => {
+    db.all(`SELECT * FROM usuarios`, [], (err, rows) => {
         if (err) {
             return res.status(500).send(err.message);
         }
@@ -36,12 +36,12 @@ app.get('/', (req, res) => {
 
 // Rota POST para adicionar uma nova comida
 app.post('/', (req, res) => {
-    const { gostosas, suculentas, image_url } = req.body;
-    db.run(`INSERT INTO comidas (gostosas, suculentas, image_url) VALUES (?, ?, ?)`, [gostosas, suculentas, image_url], function(err) {
+    const { nome, email, senha } = req.body;
+    db.run(`INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)`, [nome, email, senha], function(err) {
         if (err) {
             return res.status(500).send(err.message);
         }
-        res.status(201).send({ id: this.lastID, gostosas, suculentas, image_url });
+        res.status(201).send({ id: this.lastID, nome, email, senha });
     });
 });
 
