ace0e3019b9af03e8c5c2d966de93e869da0e459
Jurimeu <139811045+Jurimeu@users.noreply.github.com>
2024-10-18 11:17:13 -0300

    Add files via upload

---
+++ src/view/script/tela_incial.js
@@ -0,0 +1,5 @@
+// Aqui você pode adicionar funcionalidades JavaScript no futuro
+console.log("Tela inicial carregada.");
+document.getElementById('open_btn').addEventListener('click', function () {
+    document.getElementById('sidebar').classList.toggle('open-sidebar');
+});
