e73f11944ccdb547730499784607bd7b81f5027b
Jurimeu <139811045+Jurimeu@users.noreply.github.com>
2024-10-21 11:05:42 -0300

    Update tela_incial.js

--- src/view/script/tela_incial.js
+++ src/view/script/tela_incial.js
@@ -1,5 +1,47 @@
 // Aqui você pode adicionar funcionalidades JavaScript no futuro
+
+// Aqui você pode adicionar funcionalidades JavaScript no futuro
 console.log("Tela inicial carregada.");
+
+async function listarAquarios() {
+    try {
+        const response = await fetch('http://localhost:3000/aquario');
+        const aquarios = await response.json();
+        const aquariosList = document.getElementById('aquariosList');
+        aquariosList.innerHTML = '';
+
+        aquarios.forEach(aquario => {
+            const card = document.createElement('div');
+            card.className = 'project-card'; // Usando a classe project-card
+            
+            // Criando uma imagem padrão
+            const img = document.createElement('img');
+
+            img.src = '../img/Aquario_esfera.jpg'; // URL da imagem padrão
+            
+            img.alt = aquario.nome; // Texto alternativo
+            img.className = 'imgCard'; // Adiciona a classe da imagem
+            
+            // Criando o texto
+            const nome = document.createElement('p');
+            nome.textContent = aquario.nome; // Nome do aquário
+
+            // Adicionando a imagem e o texto ao card
+            card.appendChild(img);
+            card.appendChild(nome);
+
+            // Adicionando o card ao container
+            aquariosList.appendChild(card);
+        });
+    } catch (error) {
+        console.error('Erro ao listar aquários:', error);
+    }
+}
+
+// Chama a função ao carregar a página
+listarAquarios();
+
+
 document.getElementById('open_btn').addEventListener('click', function () {
     document.getElementById('sidebar').classList.toggle('open-sidebar');
 });
@@ -50,3 +92,4 @@ window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', ({ 
   theme.value = isDark ? 'dark' : 'light';
   setPreference();
 });
+
